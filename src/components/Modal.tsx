86cb68bc79aedc9522208df817684b59003b4787
shimotsu4431 <shimotsu13@gmail.com>
2021-01-23 20:40:14 +0900

    add outside click

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx'
+import { useCallback, useRef } from 'react'
 
 type Prop = {
   isOpen: boolean
@@ -6,12 +7,22 @@ type Prop = {
 }
 
 const Modal: React.FC<Prop> = ({ isOpen, handleClose }) => {
+  const overlay = useRef<HTMLDivElement>(null)
+
+  const handleClick = useCallback(() => {
+    handleClose()
+  }, [handleClose])
+
   return (
     <>
       <>
         {/* ここで {isOpen && <></>} とやるとフェードインできない */}
         <div className={clsx('Modal', isOpen && 'isShow')}>
-          <div className="Modal_overlay"></div>
+          <div
+            className="Modal_overlay"
+            ref={overlay}
+            onClick={handleClick}
+          ></div>
           <div className="Modal_container">
             <h2 className="Modal_container__title">Modal inner</h2>
             <p>モーダルの中身です</p>
