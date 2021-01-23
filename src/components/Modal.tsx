8b11b7c80d6191ba1281805f9437e223252cb37b
shimotsu4431 <shimotsu13@gmail.com>
2021-01-23 20:52:29 +0900

    fix style

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -26,7 +26,9 @@ const Modal: React.FC<Prop> = ({ isOpen, handleClose }) => {
           <div className="Modal_container">
             <h2 className="Modal_container__title">Modal inner</h2>
             <p>モーダルの中身です</p>
-            <button onClick={handleClose}>close</button>
+            <button className="Modal_container__button" onClick={handleClose}>
+              close
+            </button>
           </div>
         </div>
       </>
