e64ab2e29a7fd72d475080407cc210a59c309dc5
shimotsu4431 <shimotsu13@gmail.com>
2021-01-23 20:29:55 +0900

    fix

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,11 +9,12 @@ const Modal: React.FC<Prop> = ({ isOpen, handleClose }) => {
   return (
     <>
       <>
+        {/* ここで {isOpen && <></>} とやるとフェードインできない */}
         <div className={clsx('Modal', isOpen && 'isShow')}>
           <div className="Modal_overlay"></div>
           <div className="Modal_container">
             <h2 className="Modal_container__title">Modal inner</h2>
-            <p>{isOpen ? 'open' : 'close'}</p>
+            <p>モーダルの中身です</p>
             <button onClick={handleClose}>close</button>
           </div>
         </div>
